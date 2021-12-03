4518d3a9d66d1cde25613c489f9cd900650bcf62
antony-frolov <i.am.antonyfrolov@gmail.com>
2021-12-03 12:26:24 +0300

    Add random matrix generator functionality

--- data_generators.py
+++ data_generators.py
@@ -2,19 +2,38 @@ import numpy as np
 
 
 class DataGenerator:
-    def __init__(self, H, M, res_dist='normal', scale=0.01):
+    def __init__(self, H, M, res_dist='normal', scale=0.01, dim=None, H_dist='uniform'):
         self.H = H
-        self.dim = H.shape[0]
+        if isinstance(H, str) and H == 'random':
+            self.dim = dim
+        else:    
+            self.dim = H.shape[0]
+        
         if res_dist == 'normal':
             self.res_gen = lambda: np.random.normal(scale=scale, size=self.dim)
         else:
-            raise ValueError(f"Distribution '{res_dist}' not implemented")
+            raise ValueError(f"Residue distribution '{res_dist}' not implemented")
+        
         self.x_range = np.arange(-2*M - 1, (2*M + 1) + 1, 2)
         self.x_gen = lambda: np.random.choice(self.x_range, size=self.dim)
+        
+        self.H_dist = None
+        self.H_gen = None
+        
+        if isinstance(H, str) and H == 'random':
+            if H_dist == 'uniform':
+                self.H_dist = H_dist
+                self.H_gen = lambda: np.random.random((self.dim, self.dim))
+            else:
+                raise ValueError(f"H distribution '{H_dist}' not implemented")
 
     def generate(self, size=10):
         target = np.row_stack([self.x_gen() for _ in range(size)])
-        data = np.row_stack([self.H @ x - self.res_gen() for x in target])
+        if isinstance(self.H, str) and self.H == 'random':
+            data = {'H': np.array([self.H_gen() for _ in range(len(target))])}
+            data['y'] = np.row_stack([H @ x - self.res_gen() for H, x in zip(data['H'], target)])
+        else:
+            data = np.row_stack([self.H @ x - self.res_gen() for x in target])
         return data, target
 
 
@@ -24,25 +43,19 @@ class ADNNDataTransformer:
 
     def transform(self, data, target):
         datasets, targets = [], []
-        for i in range(data.shape[1]):
-            data_p = data - (target[:, i] - 1)[:, np.newaxis] * self.H[:, i]
-            data_n = data - (target[:, i] + 1)[:, np.newaxis] * self.H[:, i]
-            dataset = np.hstack((data_p, data_n)).reshape(-1, data.shape[1])
+        for i in range(target.shape[1]):
+            if isinstance(self.H, str) and self.H == 'random':
+                h_i = data['H'][:, :, i]
+                y = data['y']
+            else:
+                h_i = self.H[:, i]
+                y = data
+            data_p = y - (target[:, i] - 1)[:, np.newaxis] * h_i
+            data_n = y - (target[:, i] + 1)[:, np.newaxis] * h_i
+            dataset = np.hstack((data_p, data_n)).reshape(-1, y.shape[1])
             datasets.append(dataset)
             bin_target = np.zeros((len(target), 2))
             bin_target[:, 0] = 1
             bin_target = bin_target.ravel()
             targets.append(bin_target)
         return datasets, targets
-
-
-if __name__ == '__main__':
-    H = np.array([[1, 0],
-                  [0, 1]])
-    train_generator = DataGenerator(H, M=1)
-    data, target = train_generator.generate(2)
-    print(data, target, sep='\n')
-
-    adnn_transformer = ADNNDataTransformer(H)
-    datasets, targets = adnn_transformer.transform(data, target)
-    print(datasets, targets, sep='\n')
